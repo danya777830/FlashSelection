f6f406e66e3740b73b144bdb889ac0f809b70e79
danya <danya@DAC.DAC>
2022-07-23 09:55:36 +0300

    добавил файлы с заданиями

--- a.cpp
+++ a.cpp
@@ -1,23 +1,24 @@
+// подключаем необходимые библиотеки
 #include<bits/stdc++.h>
 #include<fstream>
 
 using namespace std;
 
-ifstream in("a.txt");
+ifstream in("27A.txt");
 
 int main(){
-    //Считаем данные из файла
+    //считаем данные из файла
     int n;
-    cin >> n;
+    in >> n;
     vector<int> mas(n);
     for(int i = 0; i<n; ++i)
-        cin >> mas[i];
-    int it = 1e9;
-    for(int i = 0; i<n; ++i){
+        in >> mas[i];
+    int it = 1e9; // 1e9 - такой способ записать 1000000000, в переменной it будет лежать ответ
+    for(int i = 0; i<n; ++i){ //переберем элементы
         for(int j = i+1; j<n; ++j){
             for(int q = j+1; q<n; ++q){
-                if((mas[i]+mas[j]+mas[q])%10==3){
-                    it = min(it, i+j+q);
+                if((mas[i]+mas[j]+mas[q])%10==3){ // проверка условия
+                    it = min(it, i+j+q); //если условие верное попытаемся обновить ответ
                 }
             }
         }
--- b.cpp
+++ b.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-ifstream in("a.txt");
+ifstream in("27A.txt");
 
 int main(){
     int n;
